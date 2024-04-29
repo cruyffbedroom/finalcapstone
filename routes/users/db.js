6bcf58ee5a36b04d6f767dc4c12ed2f4b5749e05
cruyffbedroom <cruyffbedroom@gmail.com>
2024-04-29 15:07:54 -0400

    users GET endpoint working

---
+++ routes/users/db.js
@@ -0,0 +1,16 @@
+import {MongoClient} from "mongodb";
+
+const connectionString = 'mongodb://127.0.0.1:27017';
+
+export const client = new MongoClient(connectionString);
+
+export async function connect() {
+
+    try {
+        await client.connect();
+        console.log('MongoDb connected');
+
+    } catch (err) {
+        console.error(err);
+    }
+}
--- routes/users/users.js
+++ routes/users/users.js
@@ -1,8 +1,23 @@
 import express from "express";
+import {client, connect} from './db.js';
+
+const dbName = 'userdb';
+const collectionName = 'users';
+
+connect();
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    res.send("GET called");    
+
+    try {
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        const prj = {user: 1, email: 1, _id: 0};
+        const users = await collection.find({}).project(prj).toArray();
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 });
 
 router.post('/', async (req, res) => {
